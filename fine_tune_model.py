0423c5368d4e4fba9121145ae336cdf2fa4a1ad5
Simon773 <simus.simon@gmail.com>
2025-11-17 18:58:05 +0100

    Update optimization of hyperparameters

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import mean_squared_error
 import wandb
 from sklearn.model_selection import KFold
 from catboost import CatBoostRegressor
@@ -23,13 +24,6 @@ X_test = pd.read_csv("data/challenge_test_features.csv", index_col=0)
 X_proc, X_test_proc, y_log = preprocess_data(X, X_test, y)
 
 
-# ------------------ Metric officielle (base 10) ------------------
-def rmsle_base10(y_true, y_pred):
-    log_true = np.log10(y_true + 1)
-    log_pred = np.log10(y_pred + 1)
-    return np.sqrt(np.mean((log_pred - log_true) ** 2))
-
-
 # ------------------ fonction W&B K-Fold ------------------
 def train_and_log(config=None):
     with wandb.init(config=config, project=PROJECT_NAME):
@@ -46,41 +40,34 @@ def train_and_log(config=None):
             y_val_f = y_log.iloc[val_idx]
 
             model = CatBoostRegressor(
-                iterations=cfg.iterations,
                 learning_rate=cfg.learning_rate,
                 depth=cfg.depth,
                 l2_leaf_reg=cfg.l2_leaf_reg,
                 loss_function="RMSE",
                 random_seed=42,
+                early_stopping_rounds=50,
                 verbose=False,
             )
 
             model.fit(X_train_f, y_train_f, eval_set=(X_val_f, y_val_f), verbose=False)
 
-            # Prédiction log
             y_val_pred_log = model.predict(X_val_f)
 
-            # Passage en échelle originale
-            y_true = np.expm1(y_val_f)
-            y_pred = np.expm1(y_val_pred_log).clip(0, None)
-
-            # Score officiel
-            score = rmsle_base10(y_true, y_pred)
+            score = mean_squared_error(y_val_f, y_val_pred_log)
             fold_scores.append(score)
 
-        # Score moyen sur les 5 folds
+        # mean score over 5 folds
         mean_score = np.mean(fold_scores)
 
-        wandb.log({"rmsle_base10": mean_score})
-        print(f" CV RMSLE_base10 = {mean_score:.5f}")
+        wandb.log({"msle": mean_score})
+        print(f" CV MSLE = {mean_score}")
 
 
-# ------------------ Configuration du sweep ------------------
+# ------------------ sweep configuraion ------------------
 sweep_config = {
     "method": "bayes",
-    "metric": {"name": "rmsle_base10", "goal": "minimize"},
+    "metric": {"name": "msle", "goal": "minimize"},
     "parameters": {
-        "iterations": {"values": [600, 800, 1000, 1200]},
         "learning_rate": {"distribution": "uniform", "min": 0.02, "max": 0.10},
         "depth": {"values": [4, 5, 6, 7]},
         "l2_leaf_reg": {"values": [1, 3, 6, 10]},
